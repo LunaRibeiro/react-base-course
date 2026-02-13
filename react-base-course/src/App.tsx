e15a5e56dd05f6f4f4b42ebde1dc29b95c77e0aa
Luna Ribeiro Alves dos Santos <luna.as@itaipuparquetec.org.br>
2026-02-13 14:30:18 -0300

    feat: remove project files and clean up repository

--- react-base-course/src/App.tsx
+++ react-base-course/src/App.tsx
@@ -1,18 +1,8 @@
-import './App.css'
-import {useState} from "react";
-
-function App() {
-    const [count, setCount] = useState(0)
+export function App() {
 
     return (
         <>
             <p>HelloWorld</p>
-
-            <button onClick={() => setCount(count + 1)}>
-                Count {count}
-            </button>
         </>
     )
 }
-
-export default App
